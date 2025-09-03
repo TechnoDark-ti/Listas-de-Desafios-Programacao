bd98e40bb648848eea4bc09ce6572f255738c2bf
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-09-03 19:49:41 -0300

    Update Q1_IF.cpp

--- c++/Q1_IF.cpp
+++ c++/Q1_IF.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-using namespace std;
-
-void Q1_IF(){
+int main(){
 	float a = 8, b = 9, c = 7;
 	double media = 0;
 
 	media = (a + b + c)/3;
 
-	cout << "A média aritmética é: " << media;
+	std::cout << "A média aritmética é: " << media << std::endl;
+
+	return 0;
 }
+
