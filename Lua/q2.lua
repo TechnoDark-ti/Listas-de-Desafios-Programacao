8f75cebdc0db59d47e52f71f6403298a3960a3c9
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-09-09 18:43:35 -0300

    Create q2.lua

---
+++ Lua/q2.lua
@@ -0,0 +1,25 @@
+-- lua 5.4
+
+function somar(entrada1, entrada2)
+    print("")
+    entrada1 = io.read()
+end
+
+function subtrair()
+end
+
+function multiplicacao()
+end
+
+while true do
+    print("")
+    io.write("Escolha: ")
+    local opcao = tonumber(io.read())
+
+
+    if opcao == 1 do
+    end
+    elseif opcao == 2 do
+    end
+else
+end
