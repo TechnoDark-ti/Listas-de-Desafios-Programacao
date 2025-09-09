53243c4466a2d375b4c5cd7654a458d24b658119
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-09-09 18:43:17 -0300

    Create q3.cpp

---
+++ c++/q3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+    int num[2];
+    int entrada1, entrada2, entrada3;
+
+    std::cin >> entrada1;
+    std::cin >> entrada2;
+    std::cin >> entrada3;
+
+    num[0] = entrada1;
+    num[1] = entrada2;
+    num[2] = entrada3;
+
+    for (int i = 0; i > 3; i++){
+        
+    }
+
+    return 0;
+}
