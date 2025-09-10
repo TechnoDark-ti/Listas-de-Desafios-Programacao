1838588434375ba8e577819254109c20c547324b
soaresdenny <104866470+soaresdenny@users.noreply.github.com>
2025-09-10 20:46:49 -0300

    Create caminho.cpp

---
+++ c++/OBI2022/caminho.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int qtd_postes, trechos_escuros = 0;
+
+  cin >> qtd_postes;
+  int postes[qtd_postes];
+
+  for (int i = 0; i < qtd_postes; i++) {
+    cin >> postes[i];
+  }
+
+  for (int j = 0; j < qtd_postes; j++) {
+    if (postes[j] != postes[qtd_postes - 1]) {
+      if (postes[j] + postes[j + 1] < 1000) {
+        trechos_escuros++;
+      }
+    }
+
+    if (postes[j] == postes[qtd_postes - 1]) {
+      if (postes[j] + postes[0] < 1000) {
+        trechos_escuros++;
+      }
+    }
+  }
+
+  cout << trechos_escuros << endl;
+  
+  return 0;
+}
