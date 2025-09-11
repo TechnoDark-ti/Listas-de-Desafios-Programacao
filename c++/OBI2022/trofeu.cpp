d9ead7eb24087c93991b9d132b58e91c7d043b4b
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-09-11 14:18:59 -0300

    Update trofeu.cpp

--- c++/OBI2022/trofeu.cpp
+++ c++/OBI2022/trofeu.cpp
@@ -17,15 +17,13 @@ int main() {
   std::vector<int> pontuacao = {0, 0, 0, 0, 0};
   int placas = 0, trofeus = 0;
 
-  for (int i = 0; i <= 4; i++)
-  {
+  for (int i = 0; i <= 4; i++){
     cin >> pontuacao[i];
   }
 
   sort(pontuacao.begin(), pontuacao.end(), greater<int>());
 
-  for (int ponto : pontuacao)
-  {
+  for (int ponto : pontuacao){
     if (ponto != pontuacao[0]) {
       break;
     }
